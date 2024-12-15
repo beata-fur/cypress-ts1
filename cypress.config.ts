3fb377367ac3236fc4a9fec3428e00497c48bbbc
beata-fur <beata.furgacz1@gmail.com>
2024-12-15 11:09:51 +0100

    ghpages create

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "cypress";
+import { allureCypress } from "allure-cypress/reporter";
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8081',
+    env: {
+      backendUrl: 'http://localhost:4001'
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.args.push('--disable-search-engine-choice-screen')
+        }
+        return launchOptions
+      })
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 });
